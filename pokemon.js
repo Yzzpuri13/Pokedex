a5da472742836dd99e91f5d4c43566110bafa660
Yzzpuri13 <83353956+Yzzpuri13@users.noreply.github.com>
2024-01-04 00:50:10 -0500

    Pokemon
    pokedex

---
+++ pokemon.js
@@ -0,0 +1,10 @@
+const MAX_POKEMON= 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+let allPokemons = [];
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit`)
